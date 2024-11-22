d0912b4fe65b1a41f4edd121f577c090d930f222
JIAN11442 <yikjian11442@gmail.com>
2024-11-22 15:11:34 +0800

    feat: scaffold search page route and basic component structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 
 import HomePage from "./pages/home.page";
 import ResetPasswordPage from "./pages/reset-password.page";
+import AppContainerPage from "./pages/app-container.page";
+import PlaylistPage from "./pages/playlist.page";
+import SearchPage from "./pages/search.page";
 
 import Sidebar from "./components/sidebar.component";
 
 import { setNavigate } from "./lib/navigate.lib";
-import AppContainerPage from "./pages/app-container.page";
-import PlaylistPage from "./pages/playlist.page";
 
 function App() {
   // original navigate function can't be using outside of the component
@@ -19,6 +20,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Sidebar />}>
         <Route index element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
         <Route path="/password/reset" element={<ResetPasswordPage />} />
 
         {/* need login to access */}
